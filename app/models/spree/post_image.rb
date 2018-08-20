2e22ab5101ca321b3205e7985cc067880a5ea4c0
Alexander Merkulov <sasha@merqlove.ru>
2018-08-20 09:35:39 +0300

    Fix image upload options

--- app/models/spree/post_image.rb
+++ app/models/spree/post_image.rb
@@ -1,11 +1,12 @@
 class Spree::PostImage < Spree::Asset
-  validates_attachment_presence :attachment
-
   has_attached_file :attachment,
-                    :styles => Proc.new{ |clip| clip.instance.attachment_sizes },
+                    :styles => lambda { |clip| clip.instance.attachment_sizes },
                     :default_style => :medium,
                     :url => '/spree/posts/:id/:style/:basename.:extension',
                     :path => ':rails_root/public/spree/posts/:id/:style/:basename.:extension'
+
+  validates_attachment :attachment,
+                       :presence => true
 
   def image_content?
     attachment_content_type.to_s.match(/\/(jpeg|png|gif|tiff|x-photoshop)/)
